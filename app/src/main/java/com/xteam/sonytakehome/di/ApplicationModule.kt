4a8d5a7cc017c0b30601bf6962b608862fea2958
Federico Perez <federico.perez@x-team.com>
2019-11-05 17:40:58 -0300

    Added cache to requests

--- app/src/main/java/com/xteam/sonytakehome/di/ApplicationModule.kt
+++ app/src/main/java/com/xteam/sonytakehome/di/ApplicationModule.kt
@@ -2,18 +2,21 @@ package com.xteam.sonytakehome.di
 
 import android.content.Context
 import android.util.Log
-import androidx.room.Room
 import com.xteam.sonytakehome.BuildConfig
 import com.xteam.sonytakehome.api.YelpService
-import com.xteam.sonytakehome.db.BusinessDao
-import com.xteam.sonytakehome.db.YelpDB
 import dagger.Module
 import dagger.Provides
+import okhttp3.Cache
+import okhttp3.CacheControl
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.io.File
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
 import javax.inject.Singleton
+
 
 @Module
 object ApplicationModule {
@@ -21,6 +24,7 @@ object ApplicationModule {
     @Provides
     @Singleton
     @JvmStatic
+    @Named("auth")
     fun provideAuthInterceptor(): Interceptor {
         return Interceptor { chain ->
             val newRequest = chain.request()
@@ -40,7 +44,28 @@ object ApplicationModule {
     @Provides
     @Singleton
     @JvmStatic
-    fun provideRetrofit(okHttpClient: OkHttpClient) = Retrofit.Builder()
+    @Named("cached")
+    fun provideCacheInterceptor(): Interceptor {
+        return Interceptor { chain ->
+
+            val response = chain.proceed(chain.request())
+
+            val cacheControl = CacheControl.Builder()
+                .maxAge(15, TimeUnit.MINUTES)
+                .build()
+
+            response.newBuilder()
+                .removeHeader("Pragma")
+                .removeHeader("Cache-Control")
+                .header("Cache-Control", cacheControl.toString())
+                .build()
+        }
+    }
+
+    @Provides
+    @Singleton
+    @JvmStatic
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
         .baseUrl(BuildConfig.BASE_URL)
         .client(okHttpClient)
         .addConverterFactory(MoshiConverterFactory.create())
@@ -49,8 +74,22 @@ object ApplicationModule {
     @Provides
     @Singleton
     @JvmStatic
-    fun provideOkHttpClient(interceptor: Interceptor) = OkHttpClient().newBuilder()
-        .addInterceptor(interceptor)
+    fun provideCache(context: Context): Cache {
+        val httpCacheDirectory = File(context.cacheDir, "yelpCache")
+        val cacheSize = 10L * 1024L * 1024L
+        return Cache(httpCacheDirectory, cacheSize)
+    }
+
+    @Provides
+    @Singleton
+    @JvmStatic
+    fun provideOkHttpClient(
+        @Named("cached") cacheInterceptor: Interceptor, @Named("auth") authInterceptor: Interceptor,
+        cache: Cache
+    ) = OkHttpClient().newBuilder()
+        .cache(cache)
+        .addNetworkInterceptor(cacheInterceptor)
+        .addInterceptor(authInterceptor)
         .build()
 
     @Provides
@@ -58,18 +97,5 @@ object ApplicationModule {
     @JvmStatic
     fun provideYelpService(retrofit: Retrofit): YelpService = retrofit
         .create(YelpService::class.java)
-
-    @Singleton
-    @Provides
-    @JvmStatic
-    fun provideDb(app: Context): YelpDB = Room
-        .databaseBuilder(app, YelpDB::class.java, "takehome.db")
-        .fallbackToDestructiveMigration()
-        .build()
-
-    @Singleton
-    @Provides
-    @JvmStatic
-    fun provideBusinessDao(db: YelpDB): BusinessDao = db.businessDao()
 
 }
--- app/src/main/java/com/xteam/sonytakehome/repository/BusinessRepository.kt
+++ app/src/main/java/com/xteam/sonytakehome/repository/BusinessRepository.kt
@@ -1,15 +1,11 @@
 package com.xteam.sonytakehome.repository
 
 import com.xteam.sonytakehome.api.YelpService
-import com.xteam.sonytakehome.db.BusinessDao
-import com.xteam.sonytakehome.db.YelpDB
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class BusinessRepository @Inject constructor(
-    private val db: YelpDB,
-    private val businessDao: BusinessDao,
     private val yelpService: YelpService
 ) {
 
